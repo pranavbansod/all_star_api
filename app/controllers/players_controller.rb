a1cacd1a5f4c40db983727dbb1ff3eb33bd3d8be
Pranav Bansod <pranavb@thoughtworks.com>
2018-10-08 16:10:56 +0530

    Added post players endpoint to create new player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,17 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all
-    render json: @players, status: :ok
+    json_response(@players)
+  end
+
+  def create
+    @player = Player.new(params.require(:player).permit(:name,:position))
+    json_response(@player,:created)
+  end
+
+
+  private
+  def json_response(object,status=:ok)
+    render json: object, status: status
   end
 end
--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -1,20 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'AllStar API', type: :request do
-  let!(:players) { create_list(:player, 10) }
+  let!(:players) {create_list(:player, 10)}
 
   describe 'GET /players' do
 
-    before {get '/players'}
+    before do
+      get '/players'
+    end
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
 
     it 'returns players' do
-      json = response.body
+      json = JSON.parse(response.body)
       expect(json).not_to be_empty
-      expect(JSON.parse(json).size).to eq(10)
+      expect(json.size).to eq(10)
+    end
+  end
+
+  describe 'POST /players' do
+
+    before do
+      post '/players', params: {player: {:name => 'Pranav', :position => 'GK'}}
+    end
+
+    it 'should create a new player' do
+      json = JSON.parse(response.body)
+      expect(json['name']).to eq('Pranav')
+    end
+
+    it 'returns status code 201' do
+      expect(response).to have_http_status(201)
     end
   end
 end
