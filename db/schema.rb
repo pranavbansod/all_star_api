ba21cc2e5e3126e87c0b4d4486028d5b2612ebd9
Pranav Bansod <pranavb@thoughtworks.com>
2018-10-11 12:47:48 +0530

    Created team model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,16 @@
-# This file is auto-generated from the player state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
-# system, you should be using db:schema:loadPlayer, not running all the migrations
+# system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_105106) do
+ActiveRecord::Schema.define(version: 2018_10_11_071613) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_10_09_105106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "number"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "rank"
+    t.string "league"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
