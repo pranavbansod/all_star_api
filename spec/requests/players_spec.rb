888476f6f105f238cb9cc85b08a3dd517771aac9
Pranav Bansod <pranavb@thoughtworks.com>
2018-10-12 09:50:57 +0530

    Fixed test of add player to a team

--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -1,13 +1,35 @@
 require 'rails_helper'
 
+def add_player(number,name,position)
+  post '/teams/1/players', params: {player: {:number => number, :name => name, :position => position}}
+end
+
 RSpec.describe 'AllStar API', type: :request do
   let!(:teams) {create_list(:team, 1)}
 
-  describe 'GET /teams/:team_id/players    - Players of a specific teams' do
+  describe 'POST /teams/:team_id/players   - Add player to a specific team' do
 
     before do
-      post '/teams/1/players', params: {player: {:number=>1,:name => 'Pranav', :position => 'GK'}}
-      post '/teams/1/players', params: {player: {:number=>4,:name => 'PATEL', :position => 'CB'}}
+      add_player(1,'Pranav','GK')
+    end
+
+    it 'should create a new player' do
+      get '/teams/1/players'
+      json = JSON.parse(response.body)
+      expect(json).not_to be_empty
+      expect(json.size).to eq(1)
+    end
+
+    it 'returns status code 201' do
+      expect(response).to have_http_status(201)
+    end
+  end
+
+  describe 'GET /teams/:team_id/players   - All Players of a specific teams' do
+
+    before do
+      add_player(1,'Pranav','GK')
+      add_player(2,'Patel','CB')
       get '/teams/1/players'
     end
 
@@ -19,24 +41,8 @@ RSpec.describe 'AllStar API', type: :request do
       json = JSON.parse(response.body)
       expect(json).not_to be_empty
       expect(json.size).to eq(2)
+      expect(json[1]['name']).to eq('Patel')
     end
   end
 
-  describe 'POST /teams/:team_id/players   - Add player to a specific team' do
-
-    before do
-      post '/teams/1/players', params: {player: {:number=>01,:name => 'Pranav', :position => 'GK'}}
-    end
-
-    it 'should create a new player' do
-      json = JSON.parse(response.body)
-      expect(json['name']).to eq('Pranav')
-
-      get '/teams/1/players'
-    end
-
-    it 'returns status code 201' do
-      expect(response).to have_http_status(201)
-    end
-  end
 end
