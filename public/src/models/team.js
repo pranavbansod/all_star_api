19c4092b3a4f292016c6865d748d84f23e3c44fb
Pranav Bansod <pranavb@thoughtworks.com>
2018-10-12 16:05:10 +0530

    Removed unnecessary code from team model.

--- public/src/models/team.js
+++ public/src/models/team.js
@@ -4,8 +4,6 @@ let team = {
     rank:'',
     name:'',
     league:'',
-    player:'',
-    players: [],
     load:function (team_id) {
         return m.request({
             method:'get',
@@ -17,43 +15,6 @@ let team = {
         })
 
     },
-    // fetchPlayers: function () {
-    //     return m.request({
-    //         method: 'get',
-    //         url: '/players'
-    //     }).then(function (players) {
-    //         team.players = players;
-    //     })
-    // },
-    // loadPlayer: function (id) {
-    //     return m.request({
-    //         method: "GET",
-    //         url: `/player/${id}`,
-    //     })
-    //         .then(function (result) {
-    //             team.player = result;
-    //         })
-    // },
-    // addPlayer: function (number, name, position) {
-    //     return m.request({
-    //         method: 'post',
-    //         url: '/players',
-    //         data: {'player': {'number': number, 'name': name, 'position': position}},
-    //     })
-    // },
-    // savePlayer:function () {
-    //     return m.request({
-    //         method: 'put',
-    //         url:`/players/${team.player.id}`,
-    //         data:team.player
-    //     })
-    // },
-    // deletePlayer:function () {
-    //     return m.request({
-    //         method:'delete',
-    //         url:`/players/${team.player.id}`
-    //     })
-    // }
 };
 
 module.exports = team;
