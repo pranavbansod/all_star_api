9d1c18e8e73e9b9ef73f15ea030a2290c193029b
Pranav Bansod <pranavb@thoughtworks.com>
2018-10-15 17:41:09 +0530

    Refactored player component and edit player form.

--- public/src/models/player.js
+++ public/src/models/player.js
@@ -1,24 +1,24 @@
 const m = require('mithril');
 
 let player = {
-    currentPlayer:{},
-    oninit:function (vnode) {
-        player.teamId = vnode.attrs.teamId;
-        player.id = vnode.attrs.playerId
-    },
+    number:'',
+    name:'',
+    position:'',
     getPlayer:function (teamId, playerId) {
         return m.request({
             method:'get',
             url:`/teams/${teamId}/players/${playerId}`
         }).then(function (result) {
-            player.currentPlayer = result;
+            player.name = result.name;
+            player.number = result.number;
+            player.position = result.position;
         })
     },
     updatePlayer:function (teamId,id) {
         return m.request({
             method: 'put',
             url:`/teams/${teamId}/players/${id}`,
-            data:{'player':player.currentPlayer}
+            data:{'player':{'name':player.name,'number':player.number,'position':player.position}}
         })
     }
 };
--- public/src/views/editPlayerForm.js
+++ public/src/views/editPlayerForm.js
@@ -1,11 +1,11 @@
 const m = require('mithril');
 
-let players = require('../models/players');
+let player = require('../models/player');
 
 let editPlayerForm = {
     oninit: function (vnode) {
         let attrs = vnode.attrs;
-        players.getPlayer(attrs.teamId, attrs.playerId)
+        player.getPlayer(attrs.teamId, attrs.playerId)
     },
     display: 'none',
     view: function (vnode) {
@@ -14,28 +14,28 @@ let editPlayerForm = {
             m("label.label", "Number"),
             m("input.number.input[type=text][placeholder=Number]", {
                 oninput:m.withAttr("value",function (number) {
-                    players.currentPlayer.number = number;
+                    player.number = number;
                 }),
-                value: players.currentPlayer.number
+                value: player.number
             }),
             m("label.label", "Name"),
             m("input.name.input[type=text][placeholder=Name]", {
                 oninput:m.withAttr("value",function (name) {
-                    players.currentPlayer.name = name;
+                    player.name = name;
                 }),
-                value: players.currentPlayer.name
+                value: player.name
             }),
             m("label.label", "Position"),
             m("input.position.input[placeholder=Position]",{
                 oninput:m.withAttr("value",function (position) {
-                    players.currentPlayer.position = position;
+                    player.position = position;
                 }),
-                value: players.currentPlayer.position
+                value: player.position
             }),
             m("br"),
             m("button.button[type=submit]",{
                 onclick:function () {
-                    players.updatePlayer(vnode.attrs.teamId,vnode.attrs.playerId).then(function () {
+                    player.updatePlayer(vnode.attrs.teamId,vnode.attrs.playerId).then(function () {
                         window.location = `#!/teams/${vnode.attrs.teamId}`
                     });
                 }
